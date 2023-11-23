e16c2c68b5d8082119beca453b8bfde009517df5
kyoungiklee <kyoungik.lee+1@gmail.com>
2023-11-23 18:02:22 +0900

    dependency 버전 업그레이드
    - spring boot 2.7.0 -> 3.1.5
    - vulnerable dependency 업그레이드

--- src/main/java/com/opennuri/study/springboot/egov/sample/common/web/EgovImgPaginationRenderer.java
+++ src/main/java/com/opennuri/study/springboot/egov/sample/common/web/EgovImgPaginationRenderer.java
@@ -1,24 +1,25 @@
 package com.opennuri.study.springboot.egov.sample.common.web;
 
 
+import jakarta.servlet.ServletContext;
 import lombok.NoArgsConstructor;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.AbstractPaginationRenderer;
 import org.springframework.lang.NonNull;
 import org.springframework.web.context.ServletContextAware;
 
-import javax.servlet.ServletContext;
+/*import javax.servlet.ServletContext;*/
 
 @NoArgsConstructor
 public class EgovImgPaginationRenderer extends AbstractPaginationRenderer implements ServletContextAware {
     
     private ServletContext servletContext;
     
-    @Override
+    /*@Override
     public void setServletContext(@NonNull ServletContext servletContext) {
         this.servletContext = servletContext;
         initVariable();
         
-    }
+    }*/
 
     private void initVariable() {
         firstPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_pre10.gif' border=0/></a>&#160;";
@@ -27,5 +28,11 @@ public class EgovImgPaginationRenderer extends AbstractPaginationRenderer implem
         otherPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">{2}</a>&#160;";
         nextPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_next1.gif' border=0/></a>&#160;";
         lastPageLabel = "<a href=\"#\" onclick=\"{0}({1}); return false;\">" + "<image src='" + servletContext.getContextPath() + "/images/egovframework/cmmn/btn_page_next10.gif' border=0/></a>&#160;";
+    }
+
+    @Override
+    public void setServletContext(jakarta.servlet.ServletContext servletContext) {
+        this.servletContext = servletContext;
+        initVariable();
     }
 }
